8e6c5ad260e6abf731ce00dc4c94f88a629530c1
zapccu <d.braner@gmx.net>
2023-05-05 16:07:05 +0200

    Bugfix

--- main.js
+++ main.js
@@ -8,6 +8,7 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = require("@iobroker/adapter-core");
+const axios = require("axios");
 
 // Load your modules here, e.g.:
 // const fs = require("fs");
